4189e256f5f81d32ac98511f0e9d48018cb30fd8
topdev101 <ritachi0502@gmail.com>
2025-02-28 13:33:09 -0500

    built the header structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-green": "#3f713f",
+      },
+    },
   },
   plugins: [],
 };
